9c2183092e2c741e8ac99cf77bd7d8f6016e9ea3
Abdesslam Mekouar <a.mekouar@outlook.com>
2023-08-29 15:37:09 -0700

    push 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height = 0;
-	size_t r_height = 0;
+	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
+	}
 	else
 	{
-		if (tree->left)
-			l_height = 1 + binary_tree_height(tree->left);
-		if (tree->right)
-			r_height = 1 + binary_tree_height(tree->right);
-
-		return ((l_height > r_height) ? l_height : r_height);
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
 	}
 }
