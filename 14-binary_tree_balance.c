3ca736c51ede523b1d054aad6b52be519610b974
Abdesslam Mekouar <a.mekouar@outlook.com>
2023-08-31 04:56:15 -0700

    push 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,21 +8,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int l_height = 0, r_height = 0;
+	int left_height = 0, right_height = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    
-    if (tree->left)
-    {
-        l_height = 1 + binary_tree_balance(tree->left);
-    }
-    if (tree->right)
-    {
-        r_height = 1 + binary_tree_balance(tree->right);
-    }
+	if (tree == NULL)
+		return (0);
 
-    return (l_height - r_height);
+	if (tree->left)
+		left_height = 1 + binary_tree_balance(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_balance(tree->right);
+
+	return (left_height - right_height);
 }
