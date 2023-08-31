24cf295e72c65816bb3856bde0b7734691329806
Abdesslam Mekouar <a.mekouar@outlook.com>
2023-08-31 04:32:01 -0700

    push 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - function that returns the number of leaves in a tree
+ * @tree: tree to check
+ * Return: number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leave = 0, left = 0, right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = binary_tree_leaves(tree->left);
+		right = binary_tree_leaves(tree->right);
+		leave = left + right;
+		return ((!left && !right) ? leave + 1 : leave + 0);
+	}
+}
